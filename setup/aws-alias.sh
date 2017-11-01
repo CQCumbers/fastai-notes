eb921a5e8785a20fa35ab8e4683d83fe2f917f4f
Alexander <alexbrianzhang@gmail.com>
2017-11-01 17:24:40 -0400

    Changed setup files to allow for running on spot instances
    Former-commit-id: 9d8e9b6e6f63c7f7b8cb22b7525d71736d167598

--- setup/aws-alias.sh
+++ setup/aws-alias.sh
@@ -1,9 +1,10 @@
-alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-lifecycle,Values=spot,Name=instance-type,Values=p2.xlarge,Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+#alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias aws-start='sh ~/Documents/fastai-notes/setup/start_spot.sh'
 alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
 alias aws-ssh='ssh -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp'
-alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
+#alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
+alias aws-stop='aws ec2 terminate-instances --instance-ids $instanceId'
 alias aws-state='aws ec2 describe-instances --instance-ids $instanceId --query "Reservations[0].Instances[0].State.Name"'
 
 
---
+++ setup/start_spot.sh
@@ -0,0 +1,138 @@
+# Parameters defaults
+# The size of the root volume, in GB.
+volume_size=128
+# The name of the key file we'll use to log into the instance. create_vpc.sh sets it to aws-key-fast-ai
+name=fast-ai
+key_name=aws-key-$name
+ami=ami-37bb714d
+subnetId=subnet-390ce364
+securityGroupId=sg-287d205a
+# Type of instance to launch
+ec2spotter_instance_type=p2.xlarge
+# In USD, the maximum price we are willing to pay.
+bid_price=1.0
+
+# Read the input args
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --ami)
+    ami="$2"
+    shift # pass argument
+    ;;
+    --subnetId)
+    subnetId="$2"
+    shift # pass argument
+    ;;
+    --securityGroupId)
+    securityGroupId="$2"
+    shift # pass argument
+    ;;
+	--volume_size)
+	volume_size="$2"
+	shift # pass argument
+	;;
+	--key_name)
+	key_name="$2"
+	shift # pass argument
+	;;
+	--ec2spotter_instance_type)
+	ec2spotter_instance_type="$2"
+	shift # pass argument
+	;;
+	--bid_price)
+	bid_price="$2"
+	shift # pass argument
+	;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # pass argument or value
+done
+
+# Create a startup script to run on instance boot
+cat >user-data.tmp <<EOF
+#!/bin/sh
+echo AWSAccessKeyId=$AWS_ACCESS_KEY > /root/.aws.creds
+echo AWSSecretKey=$AWS_SECRET_KEY >> /root/.aws.creds
+
+apt-get update
+apt-get install -y jq
+apt-get install -y python-pip python-setuptools
+apt-get install -y git
+apt-get install -y zsh
+apt-get install -y stow
+pip install awscli
+
+cd /home/ubuntu
+git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+git clone --recursive https://github.com/CQCumbers/dotfiles
+echo cloned repos
+
+rm ~/.zshrc
+cd dotfiles
+stow zsh
+usermod -s /bin/zsh ubuntu
+stow vim
+vim -E -c PlugClean -c PlugUpdate -c q
+echo loaded dotfiles
+EOF
+
+userData=$(base64 user-data.tmp | tr -d '\n');
+
+# Create a config file to launch the instance.
+cat >specs.tmp <<EOF 
+{
+  "ImageId" : "$ami",
+  "InstanceType": "$ec2spotter_instance_type",
+  "KeyName" : "$key_name",
+  "EbsOptimized": true,
+  "BlockDeviceMappings": [
+    {
+      "DeviceName": "/dev/sda1",
+      "Ebs": {
+        "DeleteOnTermination": false, 
+        "VolumeType": "gp2",
+        "VolumeSize": $volume_size 
+      }
+    }
+  ],
+  "NetworkInterfaces": [
+      {
+        "DeviceIndex": 0,
+        "SubnetId": "${subnetId}",
+        "Groups": [ "${securityGroupId}" ],
+        "AssociatePublicIpAddress": true
+      }
+  ],
+  "UserData": "${userData}"
+}
+EOF
+
+# Request the spot instance
+export requestId=`aws ec2 request-spot-instances --launch-specification file://specs.tmp --spot-price $bid_price --output="text" --query="SpotInstanceRequests[*].SpotInstanceRequestId"`
+
+echo Waiting for spot request to be fulfilled...
+aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids $requestId  
+
+# Get the instance id
+export instanceId=`aws ec2 describe-spot-instance-requests --spot-instance-request-ids $requestId --query="SpotInstanceRequests[*].InstanceId" --output="text"`
+
+echo Waiting for spot instance to start up...
+aws ec2 wait instance-running --instance-ids $instanceId
+
+echo Spot instance ID: $instanceId 
+
+# Change the instance name
+aws ec2 create-tags --resources $instanceId --tags --tags Key=Name,Value=$name-gpu-machine
+
+# Get the instance IP
+export instanceIp=`aws ec2 describe-instances --instance-ids $instanceId --filter Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+
+echo Spot Instance IP: $instanceIp
+
+# Clean up
+rm specs.tmp
+rm user-data.tmp

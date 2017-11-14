be326420e3a7162fdc4990cf9ab57895b5b81faa
Ubuntu <ubuntu@ip-10-0-0-5.ec2.internal>
2017-11-14 18:33:42 +0000

    Some unsuccessful modifications to RvB model

--- nbs/RvB.py
+++ nbs/RvB.py
@@ -13,7 +13,7 @@
 # 
 #  - Create empty scripts.txt file in appropriate directory beforehand
 
-# In[1]:
+# In[5]:
 
 
 from lxml import html
@@ -22,39 +22,62 @@ import requests, os
 path = '/home/ubuntu/fastai-data/rvb/scripts.txt'
 
 
-# In[2]:
+# In[6]:
 
 
-with open(path, 'w') as f:
-   for i in range(347):
-       page = requests.get('http://roostertooths.com/transcripts.php?eid={}'.format(i+1))
-       tree = html.fromstring(page.content)
-       lines = []
-       f.write('\n\n'+tree.xpath('//p[@class="breadcrumbs"]/a//text()')[1]
-             +'\n'+tree.xpath('//h1//text()')[0]+'\n\n')
-       for row in tree.xpath('//table[@class="script"]/tr'):
-           f.write(''.join(row.xpath('.//td//text()'))+'\n')
+def scrape():
+    with open(path, 'w') as f:
+        for i in [x for x in range(347)]:
+            page = requests.get('http://roostertooths.com/transcripts.php?eid={}'.format(i+1))
+            tree = html.fromstring(page.content)
+            lines = []
+            f.write('\n\n'+tree.xpath('//p[@class="breadcrumbs"]/a//text()')[1]
+                  +'\n'+tree.xpath('//h1//text()')[0]+'\n\n')
+            for row in tree.xpath('//table[@class="script"]/tr'):
+                f.write(''.join(row.xpath('.//td//text()'))+'\n')
+
+# scrape()
 
 
 # ## Prepare Text
 
-# In[3]:
+# In[31]:
 
 
 # imports
 from keras.models import Sequential
 from keras.layers import *
-from keras.optimizers import Adam
+from keras.optimizers import Nadam
 import numpy as np
 from IPython.display import FileLink
 
 
-# In[4]:
+# In[36]:
 
 
 # load text
 text = open(path).read().lower()[:]
-print('corpus length:', len(text))
+print('corpus length:', len(text))            
+
+
+# In[37]:
+
+
+lines = []
+for line in text.split('\n'):
+    if line.startswith(' '):
+        line = line[1:]
+        # remove last line if captioned
+        if line.startswith('caption'):
+            line = lines[-1].split(':',1)[0] + ':' + line.split(':',1)[1]
+            lines = lines[:-1]
+        line = line.split(':',1)[0].upper() + ':' + line.split(':',1)[1]
+        lines.append(line)
+text = '\n'.join(lines)
+
+
+# In[38]:
+
 
 chars = sorted(list(set(text)))
 print(chars)
@@ -68,10 +91,10 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 idx = [char_indices[c] for c in text]
 
 
-# In[5]:
+# In[40]:
 
 
-maxlen = 128
+maxlen = 64
 sentences = []
 next_chars = []
 for i in range(len(idx)-maxlen+1):
@@ -79,29 +102,29 @@ for i in range(len(idx)-maxlen+1):
     next_chars.append(idx[i+1: i+maxlen+1])
 
 
-# In[ ]:
+# In[41]:
 
 
 print('nb sequences:', len(sentences))
 print('nb chars:', len(next_chars))
 
 
-# In[ ]:
+# In[42]:
 
 
 sentences = np.concatenate([[np.array(o)] for o in sentences[:-2]])
 next_chars = np.concatenate([[np.array(o)] for o in next_chars[:-2]])
 
 
-# In[ ]:
+# In[43]:
 
 
-n_fac = 100
+n_fac = 128
 
 
 # ## Train model
 
-# In[ ]:
+# In[44]:
 
 
 from keras.models import Sequential
@@ -110,19 +133,19 @@ from keras.layers import *
 # 2 layer GRU network with 256 and 512 channels
 model = Sequential([
     Embedding(vocab_size, n_fac, input_length=maxlen),
-    CuDNNLSTM(512, input_shape=(n_fac,), return_sequences=True),
-    Dropout(0.01),
-    CuDNNLSTM(512, return_sequences=True),
-    Dropout(0.01),
+    CuDNNLSTM(640, input_shape=(n_fac,), return_sequences=True),
+    Dropout(0.4),
+    CuDNNLSTM(256, return_sequences=True),
+    Dropout(0.4),
     TimeDistributed(Dense(vocab_size)),
     Activation('softmax')
 ])
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['acc'])
+model.compile(loss='sparse_categorical_crossentropy', optimizer=Nadam(lr=0.002), metrics=['acc'])
 model.summary()
 
 
-# In[ ]:
+# In[45]:
 
 
 # save as JSON
@@ -132,13 +155,13 @@ with open('grifbot_model.json', 'w+') as f:
 FileLink('grifbot_model.json')
 
 
-# In[ ]:
+# In[46]:
 
 
 from numpy.random import choice
 import random
 
-def print_example(length=800, temp=0.75):
+def print_example(length=800, temp=0.8):
     seed_len=maxlen
     text = open(path).read().lower()[:]
     ind = random.randint(0,len(text)-seed_len-1)
@@ -158,6 +181,141 @@ def print_example(length=800, temp=0.75):
 # In[ ]:
 
 
+from keras.callbacks import *
+
+class CyclicLR(Callback):
+    """This callback implements a cyclical learning rate policy (CLR).
+    The method cycles the learning rate between two boundaries with
+    some constant frequency, as detailed in this paper (https://arxiv.org/abs/1506.01186).
+    The amplitude of the cycle can be scaled on a per-iteration or 
+    per-cycle basis.
+    This class has three built-in policies, as put forth in the paper.
+    "triangular":
+        A basic triangular cycle w/ no amplitude scaling.
+    "triangular2":
+        A basic triangular cycle that scales initial amplitude by half each cycle.
+    "exp_range":
+        A cycle that scales initial amplitude by gamma**(cycle iterations) at each 
+        cycle iteration.
+    For more detail, please see paper.
+    
+    # Example
+        ```python
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., mode='triangular')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```
+    
+    Class also supports custom scaling functions:
+        ```python
+            clr_fn = lambda x: 0.5*(1+np.sin(x*np.pi/2.))
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., scale_fn=clr_fn,
+                                scale_mode='cycle')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```    
+    # Arguments
+        base_lr: initial learning rate which is the
+            lower boundary in the cycle.
+        max_lr: upper boundary in the cycle. Functionally,
+            it defines the cycle amplitude (max_lr - base_lr).
+            The lr at any cycle is the sum of base_lr
+            and some scaling of the amplitude; therefore 
+            max_lr may not actually be reached depending on
+            scaling function.
+        step_size: number of training iterations per
+            half cycle. Authors suggest setting step_size
+            2-8 x training iterations in epoch.
+        mode: one of {triangular, triangular2, exp_range}.
+            Default 'triangular'.
+            Values correspond to policies detailed above.
+            If scale_fn is not None, this argument is ignored.
+        gamma: constant in 'exp_range' scaling function:
+            gamma**(cycle iterations)
+        scale_fn: Custom scaling policy defined by a single
+            argument lambda function, where 
+            0 <= scale_fn(x) <= 1 for all x >= 0.
+            mode paramater is ignored 
+        scale_mode: {'cycle', 'iterations'}.
+            Defines whether scale_fn is evaluated on 
+            cycle number or cycle iterations (training
+            iterations since start of cycle). Default is 'cycle'.
+    """
+
+    def __init__(self, base_lr=0.001, max_lr=0.006, step_size=2000., mode='triangular',
+                 gamma=1., scale_fn=None, scale_mode='cycle'):
+        super(CyclicLR, self).__init__()
+
+        self.base_lr = base_lr
+        self.max_lr = max_lr
+        self.step_size = step_size
+        self.mode = mode
+        self.gamma = gamma
+        if scale_fn == None:
+            if self.mode == 'triangular':
+                self.scale_fn = lambda x: 1.
+                self.scale_mode = 'cycle'
+            elif self.mode == 'triangular2':
+                self.scale_fn = lambda x: 1/(2.**(x-1))
+                self.scale_mode = 'cycle'
+            elif self.mode == 'exp_range':
+                self.scale_fn = lambda x: gamma**(x)
+                self.scale_mode = 'iterations'
+        else:
+            self.scale_fn = scale_fn
+            self.scale_mode = scale_mode
+        self.clr_iterations = 0.
+        self.trn_iterations = 0.
+        self.history = {}
+
+        self._reset()
+
+    def _reset(self, new_base_lr=None, new_max_lr=None,
+               new_step_size=None):
+        """Resets cycle iterations.
+        Optional boundary/step size adjustment.
+        """
+        if new_base_lr != None:
+            self.base_lr = new_base_lr
+        if new_max_lr != None:
+            self.max_lr = new_max_lr
+        if new_step_size != None:
+            self.step_size = new_step_size
+        self.clr_iterations = 0.
+        
+    def clr(self):
+        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
+        x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
+        if self.scale_mode == 'cycle':
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(cycle)
+        else:
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(self.clr_iterations)
+        
+    def on_train_begin(self, logs={}):
+        logs = logs or {}
+
+        if self.clr_iterations == 0:
+            K.set_value(self.model.optimizer.lr, self.base_lr)
+        else:
+            K.set_value(self.model.optimizer.lr, self.clr())        
+            
+    def on_batch_end(self, epoch, logs=None):
+        
+        logs = logs or {}
+        self.trn_iterations += 1
+        self.clr_iterations += 1
+        K.set_value(self.model.optimizer.lr, self.clr())
+
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        self.history.setdefault('iterations', []).append(self.trn_iterations)
+
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+
+
+# In[47]:
+
+
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, LambdaCallback
 import h5py
 
@@ -169,22 +327,25 @@ weight_path = "weights-{epoch:02d}.hdf5"
 checkpoint = ModelCheckpoint(os.path.join(weight_dir, weight_path),
                              monitor='acc', verbose=1, save_best_only=True, mode='max')
 reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1,
-                              patience=3, min_lr=0.000001)
+                              patience=1, min_lr=0.000001)
 printer = LambdaCallback(on_epoch_end=print_callback)
+clr = CyclicLR(base_lr=0.0001, max_lr=0.002, step_size=2000., mode='triangular')
 
-callbacks_list = [printer, checkpoint, reduce_lr]
-
-
-# In[ ]:
+callbacks_list = [printer, checkpoint, reduce_lr, clr]
 
 
-num_epochs = 7
-model.load_weights(os.path.join(weight_dir, 'weights-05.hdf5'))
+# In[48]:
+
+
+num_epochs = 32
+#model.load_weights(os.path.join(weight_dir, 'weights-02.hdf5'))
 history = []
 history.append(model.fit(sentences,
                     np.expand_dims(next_chars,-1),
+                    shuffle=True,
                     batch_size=128,
                     epochs=num_epochs,
+                    validation_split=0.15,
                     callbacks=callbacks_list))
 
 
